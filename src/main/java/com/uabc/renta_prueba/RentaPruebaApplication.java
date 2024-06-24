cf0150e39bf22bb26caf056906d4b88efa89dfb0
OmarHerrera07 <133627808+OmarHerrera07@users.noreply.github.com>
2024-06-24 09:44:54 -0700

    Project

---
+++ src/main/java/com/uabc/renta_prueba/RentaPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.uabc.renta_prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentaPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentaPruebaApplication.class, args);
+	}
+
+}
