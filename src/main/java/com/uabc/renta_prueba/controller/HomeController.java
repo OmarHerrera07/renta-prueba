abe6e0e84ffe2c849272bcb9730d3b9f38faf4be
omar.herrera13 <omar.herrera13@uabc.edu.mx>
2025-02-10 16:37:34 -0800

    Cambio

--- src/main/java/com/uabc/renta_prueba/controller/HomeController.java
+++ src/main/java/com/uabc/renta_prueba/controller/HomeController.java
@@ -2,8 +2,6 @@ package com.uabc.renta_prueba.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
 public class HomeController {
@@ -13,10 +11,11 @@ public class HomeController {
         return "index";
     }
 
+    // Comentario
     @GetMapping("/get-hola-mundo")
     public String getHolaMundo() {
         System.out.println("Hola mundo");
         return "Hola mundo";
     }
-    
+
 }
